fce3baf44504b774f7883d601d7074f13c60d3e3
francosantzz <129998263+francosantzz@users.noreply.github.com>
2023-09-10 17:17:38 -0300

    Delete ejercicio_db directory

--- ejercicio_db/src/main/java/com/utn/ejercicio_db/repositories/ConfiguracionGeneralRepository.java
+++
@@ -1,9 +0,0 @@
-package com.utn.ejercicio_db.repositories;
-
-import com.utn.ejercicio_db.entities.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ConfiguracionGeneralRepository extends JpaRepository<Cliente, Long> {
-}
